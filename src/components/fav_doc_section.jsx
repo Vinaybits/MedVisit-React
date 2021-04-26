ff5a4c99e68e94a1201eadaffad1646d445deb01
Semanticbits <faraazmohdkhan@gmail.com>
2021-04-26 15:29:27 +0530

    scroll fix

--- src/components/fav_doc_section.jsx
+++ src/components/fav_doc_section.jsx
@@ -29,105 +29,174 @@ class Doc_section extends Component {
   render() {
     return (
       <>
-        <section className="section section-doctor">
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-lg-4">
-                <div className="section-header ">
-                  <h2>Book Our Doctor</h2>
-                  <p>Lorem Ipsum is simply dummy text </p>
-                </div>
-                <div className="about-content">
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing
-                    elit, sed do eiusmod.
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing
-                    elit, sed do eiusmod tempor incididunt ut labore et{" "}
-                  </p>
-                  <a href="#!" onClick={this.doSomething}>
-                    Read More..
-                  </a>
-                </div>
-              </div>
-              <div className="col-lg-8">
-                <div className="doctor-slider slider">
-                  {this.state.doctorArray.map((item, index) => (
-                    <div className="profile-widget">
-                      <div className="doc-img">
-                        <a href="doctor-profile.html">
-                          <img
-                            className="img-fluid"
-                            alt="User"
-                            src={doc_img1}
-                          />
-                        </a>
-                        <a href="#!" className="fav-btn">
-                          <i className="far fa-bookmark"></i>
-                        </a>
-                      </div>
-                      <div className="pro-content">
-                        <h3 className="title">
-                          <a href="doctor-profile.html">{item.name}</a>
-                          <i className="fas fa-check-circle verified"></i>
-                        </h3>
-                        <p className="speciality">{item.speciality}</p>
-                        <div className="rating">
-                          <i className="fas fa-star filled"></i>
-                          <i className="fas fa-star filled"></i>
-                          <i className="fas fa-star filled"></i>
-                          <i className="fas fa-star filled"></i>
-                          <i className="fas fa-star filled"></i>
-                          <span className="d-inline-block average-rating">
-                            (17)
-                          </span>
-                        </div>
-                        <ul className="available-info">
-                          <li>
-                            <i className="fas fa-map-marker-alt"></i>{" "}
-                            {item.city},{item.state},{item.country}
-                          </li>
-                          <li>
-                            <i className="far fa-clock"></i> Available on Fri,
-                            22 Mar
-                          </li>
-                          <li>
-                            <i className="far fa-money-bill-alt"></i> $300
-                            <i
-                              className="fas fa-info-circle"
-                              data-toggle="tooltip"
-                              title="Lorem Ipsum"
-                            ></i>
-                          </li>
-                        </ul>
-                        <div className="row row-sm">
-                          <div className="col-6">
-                            <a
-                              href="doctor-profile.html"
-                              className="btn view-btn"
-                            >
-                              View Profile
-                            </a>
-                          </div>
-                          <div className="col-6">
-                            <a href="booking.html" className="btn book-btn">
-                              Book Now
-                            </a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+        <section class="section section-doctor">
+				<div class="container-fluid">
+				   <div class="row">
+						<div class="col-lg-4">
+							<div class="section-header ">
+								<h2>Book Our Doctor</h2>
+								<p>Lorem Ipsum is simply dummy text </p>
+							</div>
+							<div class="about-content">
+								<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.</p>
+								<p>web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes</p>					
+								<a href="javascript:;">Read More..</a>
+							</div>
+						</div>
+						<div class="col-lg-8">
+							<div class="doctor-slider slider">
+							
+							
+								<div class="profile-widget">
+									<div class="doc-img">
+										<a href="doctor-profile.html">
+											<img class="img-fluid" alt="User Image" src="assets/img/doctors/doctor-01.jpg"/>
+										</a>
+										<a href="javascript:void(0)" class="fav-btn">
+											<i class="far fa-bookmark"></i>
+										</a>
+									</div>
+									<div class="pro-content">
+										<h3 class="title">
+											<a href="doctor-profile.html">Ruby Perrin</a> 
+											<i class="fas fa-check-circle verified"></i>
+										</h3>
+										<p class="speciality">MDS - Periodontology and Oral Implantology, BDS</p>
+										<div class="rating">
+											<i class="fas fa-star filled"></i>
+											<i class="fas fa-star filled"></i>
+											<i class="fas fa-star filled"></i>
+											<i class="fas fa-star filled"></i>
+											<i class="fas fa-star filled"></i>
+											<span class="d-inline-block average-rating">(17)</span>
+										</div>
+										<ul class="available-info">
+											<li>
+												<i class="fas fa-map-marker-alt"></i> Florida, USA
+											</li>
+											<li>
+												<i class="far fa-clock"></i> Available on Fri, 22 Mar
+											</li>
+											<li>
+												<i class="far fa-money-bill-alt"></i> $300 - $1000 
+												<i class="fas fa-info-circle" data-toggle="tooltip" title="Lorem Ipsum"></i>
+											</li>
+										</ul>
+										<div class="row row-sm">
+											<div class="col-6">
+												<a href="doctor-profile.html" class="btn view-btn">View Profile</a>
+											</div>
+											<div class="col-6">
+												<a href="booking.html" class="btn book-btn">Book Now</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							
+						
+							
+								<div class="profile-widget">
+									<div class="doc-img">
+										<a href="doctor-profile.html">
+											<img class="img-fluid" alt="User Image" src="assets/img/doctors/doctor-02.jpg"/>
+										</a>
+										<a href="javascript:void(0)" class="fav-btn">
+											<i class="far fa-bookmark"></i>
+										</a>
+									</div>
+									<div class="pro-content">
+										<h3 class="title">
+											<a href="doctor-profile.html">Darren Elder</a> 
+											<i class="fas fa-check-circle verified"></i>
+										</h3>
+										<p class="speciality">BDS, MDS - Oral & Maxillofacial Surgery</p>
+										<div class="rating">
+											<i class="fas fa-star filled"></i>
+											<i class="fas fa-star filled"></i>
+											<i class="fas fa-star filled"></i>
+											<i class="fas fa-star filled"></i>
+											<i class="fas fa-star"></i>
+											<span class="d-inline-block average-rating">(35)</span>
+										</div>
+										<ul class="available-info">
+											<li>
+												<i class="fas fa-map-marker-alt"></i> Newyork, USA
+											</li>
+											<li>
+												<i class="far fa-clock"></i> Available on Fri, 22 Mar
+											</li>
+											<li>
+												<i class="far fa-money-bill-alt"></i> $50 - $300 
+												<i class="fas fa-info-circle" data-toggle="tooltip" title="Lorem Ipsum"></i>
+											</li>
+										</ul>
+										<div class="row row-sm">
+											<div class="col-6">
+												<a href="doctor-profile.html" class="btn view-btn">View Profile</a>
+											</div>
+											<div class="col-6">
+												<a href="booking.html" class="btn book-btn">Book Now</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							
+						
+								
+								<div class="profile-widget">
+									<div class="doc-img">
+										<a href="doctor-profile.html">
+											<img class="img-fluid" alt="User Image" src="assets/img/doctors/doctor-03.jpg"/>
+										</a>
+										<a href="javascript:void(0)" class="fav-btn">
+											<i class="far fa-bookmark"></i>
+										</a>
+									</div>
+									<div class="pro-content">
+										<h3 class="title">
+											<a href="doctor-profile.html">Deborah Angel</a> 
+											<i class="fas fa-check-circle verified"></i>
+										</h3>
+										<p class="speciality">MBBS, MD - General Medicine, DNB - Cardiology</p>
+										<div class="rating">
+											<i class="fas fa-star filled"></i>
+											<i class="fas fa-star filled"></i>
+											<i class="fas fa-star filled"></i>
+											<i class="fas fa-star filled"></i>
+											<i class="fas fa-star"></i>
+											<span class="d-inline-block average-rating">(27)</span>
+										</div>
+										<ul class="available-info">
+											<li>
+												<i class="fas fa-map-marker-alt"></i> Georgia, USA
+											</li>
+											<li>
+												<i class="far fa-clock"></i> Available on Fri, 22 Mar
+											</li>
+											<li>
+												<i class="far fa-money-bill-alt"></i> $100 - $400 
+												<i class="fas fa-info-circle" data-toggle="tooltip" title="Lorem Ipsum"></i>
+											</li>
+										</ul>
+										<div class="row row-sm">
+											<div class="col-6">
+												<a href="doctor-profile.html" class="btn view-btn">View Profile</a>
+											</div>
+											<div class="col-6">
+												<a href="booking.html" class="btn book-btn">Book Now</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							
+						
+							
+								
+							</div>
+						</div>
+				   </div>
+				</div>
+			</section>
       </>
     );
   }
